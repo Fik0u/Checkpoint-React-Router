1f9fbd65c05e39b0851b0d0d7c44c22e21adebda
Fik0u <fikouch@gmail.com>
2025-03-06 09:56:54 +0100

    Checkpoint React Router

--- src/movieData.js
+++ src/movieData.js
@@ -1,4 +1,3 @@
-// Array of objects that contains the data of the movies
 export const movieData = [
   {
     id: 1,
@@ -6,6 +5,7 @@ export const movieData = [
     rate: 4,
     description: 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=zSWdZVtXT7E'
   },
   {
     id: 2,
@@ -13,6 +13,7 @@ export const movieData = [
     rate: 4,
     description: 'A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_.jpg',
+    trailer : 'https://www.youtube.com/watch?v=PLl99DlL6b4'
 
   },
   {
@@ -21,6 +22,7 @@ export const movieData = [
     rate: 4,
     description: "Three female African-American mathematicians play a pivotal role in astronaut John Glenn's launch into orbit while dealing with racial and gender discrimination.",
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BY2RhNDRhNzctZTllMy00NTc2LWJjZmUtMzg3MTNiNjhhODMxXkEyXkFqcGc@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=5wfrDhgUMGI'
   },
   {
     id: 4,
@@ -28,6 +30,7 @@ export const movieData = [
     rate: 3,
     description: 'Geralt of Rivia, a solitary monster hunter, struggles to find his place in a world where people often prove more wicked than beasts.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BMTQ5MDU5MTktMDZkMy00NDU1LWIxM2UtODg5OGFiNmRhNDBjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    trailer:'https://www.youtube.com/watch?v=ndl1W4ltcmg'
   },
   {
     id: 5,
@@ -35,6 +38,7 @@ export const movieData = [
     rate: 3,
     description: 'Dominic Toretto and his crew of street racers plan a massive heist to buy their freedom while in the sights of a powerful Brazilian drug lord and a dangerous federal agent.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BMTUxNTk5MTE0OF5BMl5BanBnXkFtZTcwMjA2NzY3NA@@._V1_.jpg',
+    trailer:'https://www.youtube.com/watch?v=vcn2GOuZCKI'
   },
   {
     id: 6,
@@ -42,6 +46,7 @@ export const movieData = [
     rate: 4,
     description: 'Jason Bourne dodges a ruthless C.I.A. official and his Agents from a new assassination program while searching for the origins of his life as a trained killer.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BYzE3ZGM4MzctZjU5MC00NWE2LTg5ZjYtMDFiM2ZlMWQ1MjkwXkEyXkFqcGc@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=ZT2ZxjUjSo0'
   },
   {
     id: 7,
@@ -49,6 +54,7 @@ export const movieData = [
     rate: 3,
     description: 'A man who believes he has put his mysterious past behind him cannot stand idly by when he meets a young girl under the control of ultra-violent Russian gangsters.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BMTQ2MzE2NTk0NF5BMl5BanBnXkFtZTgwOTM3NTk1MjE@._V1_FMjpg_UX1000_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=VjctHUEmutw'
   },
   {
     id: 8,
@@ -56,6 +62,7 @@ export const movieData = [
     rate: 3,
     description: 'When terrorists try to seize control of a Berlin-Paris flight, a soft-spoken American co-pilot struggles to save the lives of the passengers and crew while forging a surprising connection with one of the hijackers.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BNGM1MTljMmUtMmJhYi00Y2M0LThmYjYtNGExZmJkZjNjMWIxXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=F4W5c_3_rhs'
   },
   {
     id: 9,
@@ -63,6 +70,7 @@ export const movieData = [
     rate: 4,
     description: 'Three girls are kidnapped by a man with a diagnosed 23 distinct personalities. They must try to escape before the apparent emergence of a frightful new 24th.',
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BZGI2Y2EzMGYtZDM5NS00YWExLWI5NDQtMzhjZDYxNDAzNGM2XkEyXkFqcGc@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=84TouqfIsiI'
   },
   {
     id: 10,
@@ -70,6 +78,7 @@ export const movieData = [
     rate: 3,
     description: "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as the property of the Half-Blood Prince and begins to learn more about Lord Voldemort's dark past.",
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=tAiy66Xrsz4'
   },
   {
     id: 11,
@@ -77,6 +86,7 @@ export const movieData = [
     rate: 2,
     description: "An elite Navy SEAL goes on a path to avenge his wife's murder only to find himself inside of a larger conspiracy.",
     posterUrl: 'https://m.media-amazon.com/images/M/MV5BNzE0NjU1MmUtZWUzMS00ZWRiLWE5ZjYtODk3NmRlMmExYWI1XkEyXkFqcGc@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=e-rw2cxFVLg'
   },
   {
     id: 12,
@@ -84,5 +94,6 @@ export const movieData = [
     rate: 3,
     description: 'Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.',
     posterUrl:'https://m.media-amazon.com/images/M/MV5BMGE1OWZkZmItNmVhMC00YzAxLTgxOTctNjg3NWExM2RmOWJkXkEyXkFqcGc@._V1_.jpg',
+    trailer: 'https://www.youtube.com/watch?v=dtKMEAXyPkg'
   }
 ]
